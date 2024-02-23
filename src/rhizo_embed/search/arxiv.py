aa83c345f132c3ab23b71fe958069bf56d274810
Nymph <quantnymph@gmail.com>
2024-02-22 20:38:43 -0600

    change max_results to top_n for consistency

--- src/rhizo_embed/search/arxiv.py
+++ src/rhizo_embed/search/arxiv.py
@@ -1,13 +1,13 @@
 import arxiv
 
-def general_search(query, max_results=3):
+def general_search(query, top_n=3):
     # Construct the default API client.
     client = arxiv.Client()
 
     # Search for the 10 most recent articles matching the keyword "quantum."
     search = arxiv.Search(
       query = query,
-      max_results = max_results,
+      max_results = top_n,
       sort_by = arxiv.SortCriterion.Relevance
     )
     return search.results()
