84a73cbc4f2ae8473a26c6364958555c473102b8
Nymph <quantnymph@gmail.com>
2024-02-22 20:14:33 -0600

    add arxiv search to flask api

--- src/rhizo_embed/flask_api.py
+++ src/rhizo_embed/flask_api.py
@@ -1,31 +1,44 @@
 from flask import Flask, request, jsonify
-from .search import scihub
+from .search.scholar import search_authors, search_pubs
+from .search.arxiv import general_search
 
 app = Flask(__name__)
 
-@app.route('/search/authors', methods=['GET'])
+@app.route('/scholar/authors', methods=['GET'])
 def search_authors():
-    author_name = request.args.get('author_name', '')
+    author_name = request.args.get('name', '')
     if author_name:
         try:
-            authors = scihub.search_authors(author_name)
+            authors = search_authors(author_name)
             return jsonify(authors), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 500
     else:
         return jsonify({"error": "Author name is required"}), 400
 
-@app.route('/search/publications', methods=['GET'])
+@app.route('/scholar/publications', methods=['GET'])
 def search_publications():
-    pub_title = request.args.get('pub_title', '')
+    pub_title = request.args.get('title', '')
     if pub_title:
         try:
-            publications = scihub.search_pubs(pub_title)
+            publications = search_pubs(pub_title)
             return jsonify(publications), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 500
     else:
         return jsonify({"error": "Publication title is required"}), 400
+
+@app.route('/arxiv', methods=['GET'])
+def search_arxiv():
+    query = request.args.get('query', '')
+    if query:
+        try:
+            results = general_search(query)
+            return jsonify(results), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({"error": "Query is required"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
